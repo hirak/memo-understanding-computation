8b2e9f379e61d10afedaeadb297ef18ff87fd5f8
Hiraku NAKANO <hiraku@tojiru.net>
2015-05-19 06:16:16 +0900

    BooleanとLessThanを追加

--- chapter2/SIMPLE.rb
+++ chapter2/SIMPLE.rb
@@ -61,6 +61,44 @@ class Multiply < Struct.new(:left, :right)
   end
 end
 
+class Boolean < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      LessThan.new(left.reduce, right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce)
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+end
+
 class Machine < Struct.new(:expression)
   def step
     self.expression = expression.reduce
