2f75067a48302df5a70175f1acbce940f6f755f8
Hiraku NAKANO <hiraku@tojiru.net>
2015-05-23 13:57:36 +0900

    文のevaluate

--- chapter2/SIMPLE.rb
+++ chapter2/SIMPLE.rb
@@ -278,3 +278,43 @@ class LessThan
     Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
   end
 end
+
+class Assign
+  def evaluate(environment)
+    environment.merge({ name => expression.evaluate(environment) })
+  end
+end
+
+class DoNothing
+  def evaluate(environment)
+    environment
+  end
+end
+
+class If
+  def evaluate(environment)
+    case condition.evaluate(environment)
+      when Boolean.new(true)
+        consequence.evaluate(environment)
+      when Boolean.new(false)
+        alternative.evaluate(environment)
+    end
+  end
+end
+
+class Sequence
+  def evaluate(environment)
+    second.evaluate(first.evaluate(environment))
+  end
+end
+
+class While
+  def evaluate(environment)
+    case condition.evaluate(environment)
+      when Boolean.new(true)
+        evaluate(body.evaluate(environment))
+      when Boolean.new(false)
+        environment
+    end
+  end
+end
