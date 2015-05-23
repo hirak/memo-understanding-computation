b6c96c71be99038ecba3ae25954654194334813e
Hiraku NAKANO <hiraku@tojiru.net>
2015-05-23 12:16:33 +0900

    Sequence文作成

--- chapter2/SIMPLE.rb
+++ chapter2/SIMPLE.rb
@@ -198,3 +198,27 @@ class If < Struct.new(:condition, :consequence, :alternative)
   end
 end
 
+
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def inspect
+    "《#{self}》"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    case first
+    when DoNothing.new
+      [second, environment]
+    else
+      reduced_first, reduced_environment = first.reduce(environment)
+      [Sequence.new(reduced_first, second), reduced_environment]
+    end
+  end
+end
