be3edbf09d7c94830e21b451640a01732c17d4cc
Hiraku NAKANO <hiraku@tojiru.net>
2015-05-23 13:41:38 +0900

    ビッグステップ意味論の初歩

--- chapter2/SIMPLE.rb
+++ chapter2/SIMPLE.rb
@@ -240,3 +240,41 @@ class While < Struct.new(:condition, :body)
     [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
   end
 end
+
+##### ビッグステップ意味論
+
+class Number
+  def evaluate(environment)
+    self
+  end
+end
+
+class Boolean
+  def evaluate(environment)
+    self
+  end
+end
+
+class Variable
+  def evaluate(environment)
+    environment[name]
+  end
+end
+
+class Add
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+  end
+end
+
+class Multiply
+  def evaluate(environment)
+    Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
+  end
+end
+
+class LessThan
+  def evaluate(environment)
+    Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+  end
+end
