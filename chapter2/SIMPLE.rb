7e5334a999bd77ccd01749d6248bfb254f15e850
Hiraku NAKANO <hiraku@tojiru.net>
2015-05-23 11:45:32 +0900

    仮想機械をstatementが扱える形に修正

--- chapter2/SIMPLE.rb
+++ chapter2/SIMPLE.rb
@@ -117,17 +117,17 @@ class Variable < Struct.new(:name)
   end
 end
 
-class Machine < Struct.new(:expression, :environment)
+class Machine < Struct.new(:statement, :environment)
   def step
-    self.expression = expression.reduce(environment)
+    self.statement, self.environment = statement.reduce(environment)
   end
 
   def run
-    while expression.reducible?
-      puts expression
+    while statement.reducible?
+      puts "#{statement}, #{environment}"
       step
     end
-    puts expression
+    puts "#{statement}, #{environment}"
   end
 end
 
