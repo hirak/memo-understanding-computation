3db2be4f596676af47efb1b28941ebfa82872e31
Hiraku NAKANO <hiraku@tojiru.net>
2015-06-07 14:17:59 +0900

    正規表現実装できたー

--- chapter3/REGEXP.rb
+++ chapter3/REGEXP.rb
@@ -92,10 +92,80 @@ end
 class Literal
   def to_nfa_design
     start_state = Object.new
-    accept_states = Object.new
+    accept_state = Object.new
     rule = FARule.new(start_state, character, accept_state)
     rulebook = NFARulebook.new([rule])
 
     NFADesign.new(start_state, [accept_state], rulebook)
   end
 end
+
+module Pattern
+  def matches?(string)
+    to_nfa_design.accepts?(string)
+  end
+end
+
+# Concatenateは1つ目のNFAの受理状態を全て非受理状態に変更して、次のNFAの開始状態につなげることで実現する。
+
+class Concatenate
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = first_nfa_design.start_state
+    accept_states = second_nfa_design.accept_states
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = first_nfa_design.accept_states.map { |state|
+      FARule.new(state, nil, second_nfa_design.start_state)
+    }
+
+    rulebook = NFARulebook.new(rules + extra_rules)
+    NFADesign.new(start_state, accept_states, rulebook)
+  end
+end
+
+
+# Chooseは開始状態を二つに分割し、それぞれを自由移動でつなげることで実現する。
+
+class Choose
+  def to_nfa_design
+    first_nfa_design = first.to_nfa_design
+    second_nfa_design = second.to_nfa_design
+
+    start_state = Object.new
+    accept_states = first_nfa_design.accept_states + second_nfa_design.accept_states
+    rules = first_nfa_design.rulebook.rules + second_nfa_design.rulebook.rules
+    extra_rules = [first_nfa_design, second_nfa_design].map { |nfa_design|
+      FARule.new(start_state, nil, nfa_design.start_state)
+    }
+    rulebook = NFARulebook.new(rules + extra_rules)
+
+    NFADesign.new(start_state, accept_states, rulebook)
+  end
+end
+
+
+# Repeatは
+#  - 受理状態から開始状態への自由移動追加により、a+にマッチさせる。
+#  - 受理状態でも開始状態を新しく追加し、そこから元の開始状態への自由移動を追加。空文字マッチ
+#
+
+class Repeat
+  def to_nfa_design
+    pattern_nfa_design = pattern.to_nfa_design
+
+    start_state = Object.new
+    accept_states = pattern_nfa_design.accept_states + [start_state]
+    rules = pattern_nfa_design.rulebook.rules
+    extra_rules =
+      pattern_nfa_design.accept_states.map { |accept_state|
+        FARule.new(accept_state, nil, pattern_nfa_design.start_state)
+      } +
+      [FARule.new(start_state, nil, pattern_nfa_design.start_state)]
+    rulebook = NFARulebook.new(rules + extra_rules)
+
+    NFADesign.new(start_state, accept_states, rulebook)
+  end
+end
+
--- chapter3/REGEXP_run.rb
+++ chapter3/REGEXP_run.rb
@@ -21,3 +21,52 @@ nfa_design = Literal.new('a').to_nfa_design
 puts nfa_design.accepts?('')
 puts nfa_design.accepts?('a')
 puts nfa_design.accepts?('b')
+
+puts '--------matches---------'
+puts Empty.new.matches?('a')
+puts Literal.new('a').matches?('a')
+
+puts '------- Concatenate ------------'
+pattern = Concatenate.new(
+  Literal.new('a'),
+  Concatenate.new(Literal.new('b'), Literal.new('c'))
+)
+
+puts pattern.inspect
+
+puts pattern.matches?('a')
+puts pattern.matches?('ab')
+puts pattern.matches?('abc')
+
+
+puts '------- Choose ----------------'
+
+pattern = Choose.new(Literal.new('a'), Literal.new('b'))
+puts pattern.inspect
+
+puts pattern.matches? 'a'
+puts pattern.matches? 'b'
+puts pattern.matches? 'c'
+
+
+puts '------- Repeat --------'
+
+pattern = Repeat.new(Literal.new('a'))
+puts pattern.inspect
+
+puts pattern.matches? ''
+puts pattern.matches? 'a'
+puts pattern.matches? 'aaaa'
+puts pattern.matches? 'b'
+
+puts '------- 複雑なパターン -------------'
+pattern =
+  Repeat.new(
+    Concatenate.new(
+      Literal.new('a'),
+      Choose.new(Empty.new, Literal.new('b'))
+    )
+)
+
+puts pattern.inspect
+
