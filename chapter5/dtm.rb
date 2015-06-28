f3cffa187de5962c62dbcc4fc73277cd44496605
Hiraku NAKANO <hiraku@tojiru.net>
2015-06-28 14:53:59 +0900

    DTMRulebookまで実装

--- chapter5/dtm.rb
+++ chapter5/dtm.rb
@@ -15,3 +15,39 @@ class Tape < Struct.new(:left, :middle, :right, :blank)
     Tape.new(left + [middle], right.first || blank, right.drop(1), blank)
   end
 end
+
+class TMConfiguration < Struct.new(:state, :tape)
+end
+
+class TMRule < Struct.new(:state, :character, :next_state,
+                          :write_character, :direction)
+  def applies_to?(configuration)
+    state == configuration.state && character == configuration.tape.middle
+  end
+
+  def follow(configuration)
+    TMConfiguration.new(next_state, next_tape(configuration))
+  end
+
+  def next_tape(configuration)
+    written_tape = configuration.tape.write(write_character)
+
+    case direction
+    when :left
+      written_tape.move_head_left
+    when :right
+      written_tape.move_head_right
+    end
+  end
+end
+
+
+class DTMRulebook < Struct.new(:rules)
+  def next_configuration(configuration)
+    rule_for(configuration).follow(configuration)
+  end
+
+  def rule_for(configuration)
+    rules.detect { |rule| rule.applies_to?(configuration) }
+  end
+end
--- chapter5/dtm_run.rb
+++ chapter5/dtm_run.rb
@@ -8,4 +8,31 @@ p tape.write('0')
 p tape.move_head_right
 p tape.move_head_right.write('0')
 
+p '-------------TMRule--------------'
 
+rule = TMRule.new(1, '0', 2, '1', :right)
+p rule
+
+p rule.applies_to?(TMConfiguration.new(1, Tape.new([], '0', [], '_')))
+p rule.applies_to?(TMConfiguration.new(1, Tape.new([], '1', [], '_')))
+p rule.applies_to?(TMConfiguration.new(2, Tape.new([], '0', [], '_')))
+
+p rule.follow(TMConfiguration.new(1, Tape.new([], '0', [], '_')))
+
+p '-------------DTMRulebook---------------'
+
+rulebook = DTMRulebook.new([
+  TMRule.new(1, '0', 2, '1', :right),
+  TMRule.new(1, '1', 1, '0', :left),
+  TMRule.new(1, '_', 2, '1', :right),
+  TMRule.new(2, '0', 2, '0', :right),
+  TMRule.new(2, '1', 2, '1', :right),
+  TMRule.new(2, '_', 3, '_', :left),
+])
+
+p rulebook
+
+configuration = TMConfiguration.new(1, tape)
+p configuration = rulebook.next_configuration(configuration)
+p configuration = rulebook.next_configuration(configuration)
+p configuration = rulebook.next_configuration(configuration)
