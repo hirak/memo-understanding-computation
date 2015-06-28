0bba4d8bb6b01bfad3112bdad78125fbc219e9c0
Hiraku NAKANO <hiraku@tojiru.net>
2015-06-28 11:45:12 +0900

    非決定性プッシュダウン・オートマトンについて記述開始

--- chapter4/PDARule_run.rb
+++ chapter4/PDARule_run.rb
@@ -62,9 +62,25 @@ puts dpda_design.accepts?('(((((())))))')
 puts dpda_design.accepts?('()()()()(((((()()))))')
 puts dpda_design.accepts?('((()()()()((()()))))(')
 
-puts '-------------design?-----------------'
-dpda = DPDA.new(PDAConfiguration.new(1, Stack.new(['$'])), [1], rulebook)
-dpda.read_string('())')
-puts dpda.current_configuration
+#puts '-------------design?-----------------'
+#dpda = DPDA.new(PDAConfiguration.new(1, Stack.new(['$'])), [1], rulebook)
+#dpda.read_string('())')
+#puts dpda.current_configuration
 
 #puts dpda_design.accepts?('())')
+#
+puts '-----ab-----'
+rulebook = DPDARulebook.new([
+  PDARule.new(1, 'a', 2, '$', ['a', '$']),
+  PDARule.new(1, 'b', 2, '$', ['b', '$']),
+  PDARule.new(2, 'a', 2, 'a', ['a', 'a']),
+  PDARule.new(2, 'b', 2, 'b', ['b', 'b']),
+  PDARule.new(2, 'a', 2, 'b', []),
+  PDARule.new(2, 'b', 2, 'a', []),
+  PDARule.new(2, nil, 1, '$', ['$'])
+])
+
+dpda_design = DPDADesign.new(1, '$', [1], rulebook)
+puts dpda_design.accepts?('ababab')
+puts dpda_design.accepts?('bbbaaaab')
+puts dpda_design.accepts?('baa')
