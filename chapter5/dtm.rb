c77205f58a766b08bf93296d0b238d3c6542a33f
Hiraku NAKANO <hiraku@tojiru.net>
2015-06-28 15:58:38 +0900

    チューリングマシンのシミュレーションができた

--- chapter5/dtm.rb
+++ chapter5/dtm.rb
@@ -50,4 +50,27 @@ class DTMRulebook < Struct.new(:rules)
   def rule_for(configuration)
     rules.detect { |rule| rule.applies_to?(configuration) }
   end
+
+  def applies_to?(configuration)
+    !rule_for(configuration).nil?
+  end
 end
+
+class DTM < Struct.new(:current_configuration, :accept_states, :rulebook)
+  def accepting?
+    accept_states.include?(current_configuration.state)
+  end
+
+  def step
+    self.current_configuration = rulebook.next_configuration(current_configuration)
+  end
+
+  def run
+    step until accepting? || stuck?
+  end
+
+  def stuck?
+    !accepting? && !rulebook.applies_to?(current_configuration)
+  end
+end
+
